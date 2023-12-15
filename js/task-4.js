88dca848269d0e2ac04ef265624fda81a76a9ca4
DrunkCherry1997 <142116152+DrunkCherry1997@users.noreply.github.com>
2023-12-15 20:29:22 +0100

    commie on last tasks

---
+++ js/task-4.js
@@ -0,0 +1,20 @@
+const registerForm = document.querySelector('.login-form');
+
+registerForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+    
+    if (!email.trim() || !password.trim()) {
+        alert('All form fields must be filled in');
+        return;
+    }
+
+    console.log(`Email: ${email.trim()}, Password: ${password.trim()}`);
+    form.reset();
+    alert('Form submitted successfully!');
+}
+
