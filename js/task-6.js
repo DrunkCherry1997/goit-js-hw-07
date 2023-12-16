ead0dc312e68703e5153b90b0f9cfed41022ca5d
DrunkCherry1997 <142116152+DrunkCherry1997@users.noreply.github.com>
2023-12-16 10:42:33 +0100

    commite 5th

--- js/task-6.js
+++ js/task-6.js
@@ -1,24 +1,22 @@
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, '0')}`;
+    .padStart(6, 0)}`;
 }
 
-const input = document.querySelector('input'); // Поле вводу
-const createButton = document.querySelector('[data-create]'); // Кнопка "Create"
-const destroyButton = document.querySelector('[data-destroy]'); // Кнопка "Destroy"
-const boxesContainer = document.querySelector('#boxes'); // Контейнер для блоків
+
+const input = document.querySelector('input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
 
 createButton.addEventListener('click', createBoxes);
 destroyButton.addEventListener('click', destroyBoxes);
 
 
 function createBoxes() {
-
   const amount = input.value;
-
-
   if (amount < 1 || amount > 100) {
     alert('Please enter a number between 1 and 100.');
     return;
@@ -26,16 +24,14 @@ function createBoxes() {
 
 
   destroyBoxes();
-
   let size = 30;
-
   for (let i = 0; i < amount; i++) {
     const box = document.createElement('div');
     box.style.width = `${size}px`;
     box.style.height = `${size}px`;
     box.style.backgroundColor = getRandomHexColor();
     boxesContainer.appendChild(box);
-    size += 10; 
+    size += 10;
   }
 
 
